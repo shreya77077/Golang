b8188edd8cbd089285978770530e8a6169576f35
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-02-08 23:12:10 +0530

    first commit

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Variables")
+
+	var username  string = "itesh"
+	println(username)
+	fmt.Printf("variables is of type %T \n",username) //placeholder 
+
+	var isLoggedin  bool = true
+	println(isLoggedin)
+	fmt.Printf("variables is of type %T \n",username) //placeholder 
+}
