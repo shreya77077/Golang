d39e51d24e9012c07ea39d2f99c3a118323477f0
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-02-11 14:22:02 +0530

    init

--- variables.go
+++ variables.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const LoginToken string = "hhhh" //public = L (capital)
+
 func main(){
 	fmt.Println("Variables")
 
@@ -12,4 +14,17 @@ func main(){
 	var isLoggedin  bool = true
 	println(isLoggedin)
 	fmt.Printf("variables is of type %T \n",username) //placeholder 
+
+	var smallvalue  uint8 = 255
+	println(smallvalue)
+	fmt.Printf("variables is of type %T \n",smallvalue) //placeholder 
+
+	var smallvaluefloat  float32 = 255.333348565748
+	println(smallvaluefloat)
+	fmt.Printf("variables is of type %T \n",smallvaluefloat) //placeholder 
+
+	numberofuserss := 3000
+	fmt.Println(numberofuserss ) //inside the method
+
+	fmt.Println(LoginToken)
 }
